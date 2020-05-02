c94c6e38c852c5f68fe0881f5338d1463b5ea560
Karthick Selvaraj <karthick@mallow-tech.com>
2020-05-02 21:54:04 +0530

    SUImagePickerView.swift file and usage logics added.

--- SUImagePickerView/ContentView.swift
+++ SUImagePickerView/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var image: Image? = Image("karthick")
+    @State private var shouldPresentImagePicker = false
+    @State private var shouldPresentActionScheet = false
+    @State private var shouldPresentCamera = false
+    
     var body: some View {
-        Text("Hello, World!")
+        // WARNING: Force wrapped image for demo purpose
+        image!
+            .resizable()
+            .frame(width: 300, height: 300)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+            .onTapGesture { self.shouldPresentActionScheet = true }
+            .sheet(isPresented: $shouldPresentImagePicker) {
+                SUImagePickerView(sourceType: self.shouldPresentCamera ? .camera : .photoLibrary, image: self.$image, isPresented: self.$shouldPresentImagePicker)
+        }.actionSheet(isPresented: $shouldPresentActionScheet) { () -> ActionSheet in
+            ActionSheet(title: Text("Choose mode"), message: Text("Please choose your preferred mode to set your profile image"), buttons: [ActionSheet.Button.default(Text("Camera"), action: {
+                self.shouldPresentImagePicker = true
+                self.shouldPresentCamera = true
+            }), ActionSheet.Button.default(Text("Photo Library"), action: {
+                self.shouldPresentImagePicker = true
+                self.shouldPresentCamera = false
+            }), ActionSheet.Button.cancel()])
+        }
     }
 }
 
---
+++ SUImagePickerView/SUImagePickerView.swift
@@ -0,0 +1,57 @@
+//
+//  SUImagePickerView.swift
+//  SUImagePickerView
+//
+//  Created by Karthick Selvaraj on 02/05/20.
+//  Copyright © 2020 Karthick Selvaraj. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct SUImagePickerView: UIViewControllerRepresentable {
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var image: Image?
+    @Binding var isPresented: Bool
+    
+    func makeCoordinator() -> ImagePickerViewCoordinator {
+        return ImagePickerViewCoordinator(image: $image, isPresented: $isPresented)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let pickerController = UIImagePickerController()
+        pickerController.sourceType = sourceType
+        pickerController.delegate = context.coordinator
+        return pickerController
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        // Nothing to update here
+    }
+
+}
+
+class ImagePickerViewCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @Binding var image: Image?
+    @Binding var isPresented: Bool
+    
+    init(image: Binding<Image?>, isPresented: Binding<Bool>) {
+        self._image = image
+        self._isPresented = isPresented
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.image = Image(uiImage: image)
+        }
+        self.isPresented = false
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.isPresented = false
+    }
+    
+}
+
