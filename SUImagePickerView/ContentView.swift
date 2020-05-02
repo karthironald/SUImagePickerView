ebb25dd8ec8ab7fc82e96089bf4c47d54632018f
Karthick Selvaraj <karthick@mallow-tech.com>
2020-05-02 21:46:11 +0530

    Initial commit.

---
+++ SUImagePickerView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SUImagePickerView
+//
+//  Created by Karthick Selvaraj on 02/05/20.
+//  Copyright © 2020 Karthick Selvaraj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
